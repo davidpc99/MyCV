75314a67926fc2f00c13eb3858ac4ded59a90318
davidpc99 <71788370+davidpc99@users.noreply.github.com>
2024-10-25 22:22:15 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://davidpc99.github.io',
-  base: 'MyCV',
+  base: '/MyCV',
 });
